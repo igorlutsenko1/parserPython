538886487032bc92f86d28fcb635afab040f8333
Igor Lutsenko <lutsenko99@gmail.com>
2020-07-11 22:16:29 +0500

    Customize parser v.4. Checking Teams on match page

--- parser.py
+++ parser.py
@@ -1,6 +1,8 @@
-import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import time
 
 
@@ -11,59 +13,65 @@ def main():
     options = webdriver.ChromeOptions()
     options.add_argument('user-data-dir=' + opera_profile)
     options._binary_location = r'C:\\Users\\lutse\\AppData\\Local\\Programs\Opera\\69.0.3686.57\\opera.exe'
-    driver = webdriver.Opera(executable_path=r'C:\\Users\\lutse\\Desktop\\try\\Library\\operadriver.exe', options=options)
+    driver = webdriver.Opera(executable_path=r'C:\\Users\\lutse\\Desktop\\try\\Library\\operadriver.exe',
+                             options=options)
     driver.get('https://www.pinnacle.se/ru/esports/matchups/highlights')
     driver.maximize_window()
-    time.sleep(15)
+    timeout = 5
+
+    # Ждем загрузки основной страницы с матчами
+    while True:
+        try:
+            element_present = EC.presence_of_all_elements_located((By.CLASS_NAME, 'style_container__uaHPr'))
+            WebDriverWait(driver, timeout).until(element_present)
+            break
+        except TimeoutException:
+            print("Timed out waiting for page to load")
 
     # все матчи в актуальном
     all_actual_matches = driver.find_elements_by_class_name('style_container__uaHPr')
 
+    # Проходимся по всем матчам из актуального
     counter = 0
-    for actual in all_actual_matches:
+    while counter <= len(all_actual_matches) - 1:
+        while True:
+            try:
+                element_present = EC.presence_of_all_elements_located((By.CLASS_NAME, 'style_container__uaHPr'))
+                WebDriverWait(driver, timeout).until(element_present)
+                break
+            except TimeoutException:
+                print("Timed out waiting for page to load")
         all_actual_matches = driver.find_elements_by_class_name('style_container__uaHPr')
+        all_actual_matches = [x for x in all_actual_matches if len(x.text) != 0]
+        print()
         all_actual_matches[counter].click()
         counter += 1
-        time.sleep(10)
-        teams = driver.find_elements_by_class_name('style_col3__1pR1d')
-        teams[-1].click()
-        bar = driver.find_elements_by_class_name('style_titleText__jlbrV')
-        values = driver.find_elements_by_class_name('label')
-        prices = driver.find_elements_by_class_name('price')
 
-        values_prices = {}
+        while True:
+            try:
+                element_present = EC.presence_of_all_elements_located((By.CLASS_NAME, 'style_container__1MuSF'))
+                WebDriverWait(driver, timeout).until(element_present)
+                break
+            except TimeoutException:
+                print("Timed out waiting for page to load")
+        teams = driver.find_elements_by_class_name('style_flexButton__2bj5t')
+        teams = [x for x in teams if len(x.text) != 0]
+        teams_checker = [x.text for x in teams if len(x.text) != 0]
+        if 'Teams' not in teams_checker:
+            print(f'Парс для матча {counter} не выполнен, нет вкладки Teams')
+            driver.back()
+            counter += 1
+            continue
+        else:
+            teams[-1].click()
 
-        for i in range(len(bar)):
-            name_match = bar[i].text
-            values_prices[name_match] = {values[i].text: prices[i].text, values[i+1].text: prices[i+1].text}
-        print(values_prices[2:])
+        print(f"Парс для команды {counter} выполнен!")
 
-
-
-
-        variable = 1
-
-        # Проходимся по данным и выводим в консоль
-        for i in values:
-
-            meta = values.index(i)
-
-            if meta % 2 == 0 and meta > 3:
-                print(bar[variable].text)
-                variable += 1
-                order = i.text
-                order_price = prices[meta].text
-                print(f"{order} - {order_price}  ||  {i.text} - {prices[meta + 1].text}")
-            elif meta % 2 != 0 and meta > 3:
-                print()
-        print('МАТЧ ПРОЙДЕН')
-        print('МАТЧ ПРОЙДЕН')
-        print()
         driver.back()
-        time.sleep(10)
 
     time.sleep(25)
     driver.quit()
+
 
 if __name__ == '__main__':
     main()

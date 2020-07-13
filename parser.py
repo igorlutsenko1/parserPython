cd410427f5b894661dd1d18eec533d31dcb2466b
Igor Lutsenko <lutsenko99@gmail.com>
2020-07-13 13:41:05 +0500

    Was added: league, match name, errors removed

--- parser.py
+++ parser.py
@@ -3,8 +3,6 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-import os
 import getpass
 import time
 
@@ -13,6 +11,8 @@ def main():
 
     path = input("Укажите путь до драйвера оперы, например C:\\Users\\lutse\\Desktop\\try\\Library\\operadriver.exe: ")
     name = getpass.getuser()
+    discipline = input("По какой дисциплине парсить данные? csgo / dota: ")
+    print()
 
     # настраиваем браузер и запускаем первую страницу
     opera_profile = f'C:\\Users\\{name}\\AppData\\Roaming\\Opera Software\\Opera Stable'
@@ -29,7 +29,7 @@ def main():
 
     driver.get('https://www.pinnacle.se/ru/esports/matchups/highlights')
     driver.maximize_window()
-    timeout = 5
+    timeout = 10
 
     # Ждем загрузки основной страницы с матчами
     while True:
@@ -38,20 +38,17 @@ def main():
             element_present = EC.presence_of_all_elements_located((By.CLASS_NAME, 'style_container__uaHPr'))
             WebDriverWait(driver, timeout).until(element_present)
             break
-
         except TimeoutException:
             print("Timed out waiting for page to load")
 
-
     # все матчи в актуальном
-    all_actual_matches = driver.find_elements_by_class_name('style_container__uaHPr')
+    list_of_matches = driver.find_elements_by_class_name('style_container__uaHPr')
 
-    # Проходимся по всем матчам из актуального
+    # Проходим по всем матчам
     counter = 0
-    while counter <= len(all_actual_matches) - 1:
+    while counter <= len(list_of_matches) - 1:
 
         while True:
-
             try:
                 element_present = EC.presence_of_all_elements_located((By.CLASS_NAME, 'style_container__uaHPr'))
                 WebDriverWait(driver, timeout).until(element_present)
@@ -61,25 +58,18 @@ def main():
                 print("Timed out waiting for page to load")
 
         all_actual_matches = driver.find_elements_by_class_name('style_container__uaHPr')
-        all_actual_matches = [x for x in all_actual_matches if len(x.text) != 0]
-
-       # driver.execute_script("window.scrollBy(0, 50)", "")
         working_link = all_actual_matches[counter].get_attribute('href')
-
-        actual_match = driver.find_element_by_xpath(f'//*[@id="root"]/div/div/div[2]/main/div/div/div/div[2]/div/div/div[{counter + 2}]/div[1]/a')
-        #driver.execute_script("window.scrollBy(0, 100)", "")
         link = working_link.split('/')
-
         finding_csgo = []
 
         for i in link:
             new_link = i.split('-')
-            if 'csgo' in new_link:
+            if discipline in new_link:
                 finding_csgo.append(new_link)
                 break
+
         if finding_csgo:
             driver.get(working_link)
-
         else:
             counter += 1
             continue
@@ -90,7 +80,6 @@ def main():
                 element_present = EC.presence_of_all_elements_located((By.CLASS_NAME, 'style_flexButton__2bj5t'))
                 WebDriverWait(driver, timeout).until(element_present)
                 break
-
             except TimeoutException:
                 print("Timed out waiting for page to load")
 
@@ -99,19 +88,14 @@ def main():
         teams_checker = [x.text for x in teams if len(x.text) != 0]
 
         if 'Teams' not in teams_checker:
-
             print(f'Парс для матча {counter + 1} не выполнен, нет вкладки Teams')
             print('_' * 50)
             driver.back()
             counter += 1
             continue
-
         elif 'Teams' in teams_checker:
-
             teams[-1].click()
-
         else:
-
             driver.back()
             continue
 
@@ -122,14 +106,22 @@ def main():
         count_for_content = 0
         labels = driver.find_elements_by_class_name('style_title__2Y8r7')
         content = driver.find_elements_by_class_name('style_content__eJ182')
+        teams_name = driver.find_elements_by_class_name('style_participants__3SkHT')
+        teams_name = [x.text for x in teams_name]
+        teams_name = teams_name[0].splitlines()
         content = [x.text for x in content]
+        league_name = driver.find_elements_by_class_name('style_desktop_textLabel__2RMmF')
+        league_name = [x.text for x in league_name]
+
+        print(f"{teams_name[1]} VS {teams_name[2]}")
+        print(f"League - {league_name[2]}")
+        print()
 
         for element_label in labels:
-
             print(element_label.text)
-            # print(f"content - {content[count_for_content].splitlines()}")
             content_for_element_label = content[count_for_content].splitlines()
             i = 0
+
             while i + 1 < len(content_for_element_label):
                 print(f"{content_for_element_label[i]} - {content_for_element_label[i + 1]}")
                 i += 2
@@ -140,7 +132,8 @@ def main():
         print('_' * 50)
         driver.back()
 
-    time.sleep(2)
+    print(f"Все матчи по {discipline} пройдены и загружены")
+    time.sleep(5)
     driver.quit()
 
 
